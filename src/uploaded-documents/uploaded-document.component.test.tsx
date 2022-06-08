f23fc5f518c896e0f2622e429f0bc94abfe32e2d
Elliot Evans <ell15evans.nuls@googlemail.com>
2022-06-08 23:16:22 +0100

    feat(): created service returns mock data.
    Wrote test to test service gets called and returns mock data.

--- src/uploaded-documents/uploaded-document.component.test.tsx
+++ src/uploaded-documents/uploaded-document.component.test.tsx
@@ -1,19 +1,53 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import {render, waitFor} from '@testing-library/react'
 
+import { getAllAdminUploads } from './uploaded-document.service'
 import UploadedDocument from './uploaded-document.component'
+
+jest.mock('./uploaded-document.service')
+
+const mockGetAllAdminUploads = getAllAdminUploads as jest.MockedFunction<
+    typeof getAllAdminUploads
+>
+const mockedUploadedDocuments = [
+    {
+        type: 'pdf',
+        name: 'Employee Handbook',
+        added: '2017-01-06',
+    },
+]
 
 describe('Uploaded Document', () => {
     describe('When the component has loaded', () => {
-        afterAll(() => {
+        beforeEach(() => {
+            mockGetAllAdminUploads.mockResolvedValue(mockedUploadedDocuments)
+        })
+
+        afterEach(() => {
             jest.clearAllMocks()
         })
 
-        test('Then the `Admin Uploaded` heading appears', () => {
+        test('Then the `Admin Uploaded` heading appears', async () => {
             const { getByText } = render(<UploadedDocument />)
             const heading = getByText('Admin Uploaded')
 
             expect(heading).toBeInTheDocument()
+        })
+
+        test('Then the `uploaded` endpoint has been called', async () => {
+            render(<UploadedDocument />)
+
+            expect(mockGetAllAdminUploads).toHaveBeenCalled()
+        })
+
+        test('Then the `uploaded` items appear on the page', async () => {
+            const { getByText } = render(<UploadedDocument />)
+
+            await waitFor(() => {
+                expect(
+                    getByText(mockedUploadedDocuments[0].name)
+                ).toBeInTheDocument()
+            })
         })
     })
 })
--- src/uploaded-documents/uploaded-document.component.tsx
+++ src/uploaded-documents/uploaded-document.component.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const UploadedDocument = () => (
-    <section>
-        <h1>Admin Uploaded</h1>
-    </section>
-)
+import { getAllAdminUploads } from './uploaded-document.service'
+
+const UploadedDocument = () => {
+    const [adminUploadedItems, setAdminUploadedItems] = useState<any[]>([])
+
+    useEffect(() => {
+        const allAdminUploads = async () => {
+            try {
+                const adminUploads = await getAllAdminUploads()
+                setAdminUploadedItems(adminUploads)
+            } catch (e) {
+                console.error('failed to get admin uploads')
+            }
+
+        }
+
+        allAdminUploads()
+    }, [])
+
+    return (
+        <section>
+            <h1>Admin Uploaded</h1>
+            {adminUploadedItems.map((uploadedItem, index) => (
+                <div>{uploadedItem.name}</div>
+            ))}
+        </section>
+    )
+}
 
 export default UploadedDocument
